aae48abf314589dc4f6eb2ec2eede5321b570766
sobiya amreen <asobiya9@gmail.com>
2023-12-08 16:46:55 +0530

    all responsive changes

--- src/Components/DashBoard/CardBanner/CardBannerData.js
+++ src/Components/DashBoard/CardBanner/CardBannerData.js
@@ -11,7 +11,7 @@ export const responsive = {
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2
+      items: 1
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
